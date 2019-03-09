c133d00f5af2ed63df86bff2282481c31904a498
dsaltares <dsaltares@bloomberg.net>
2019-03-09 09:52:43 +0000

    Trivial implementation of tracks request

--- src/server.js
+++ src/server.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const fs = require('fs');
+const { promisify } = require('util');
+
+const readdir = promisify(fs.readdir);
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
     res.json({
         message: 'It works!'
+    });
+});
+
+router.get('/tracks', (_req, res) => {
+    readdir('static').then(files => {
+        const tracks = files.filter(file => {
+            return file.endsWith('.mp3');
+        }).map(file => {
+            return { file };
+        });
+        res.json({ tracks });
+    }).catch(err => {
+        const errorMsg = 'Failed to get tracks';
+        res.json({
+            error: {
+                msg: errorMsg
+            }
+        });
+        console.log(errorMsg, err);
     });
 });
 
