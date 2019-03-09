6dd86df8dc4f49369589eb7da046faf49bf1a460
dsaltares <dsaltares@bloomberg.net>
2019-03-09 10:32:39 +0000

    Add tracks and track requests with a very dummy implementation

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const musicMetadata = require('music-metadata');
 const { promisify } = require('util');
 
 const readdir = promisify(fs.readdir);
+const exists = promisify(fs.exists);
 
 const router = express.Router();
+const tracksFolder = 'static';
 
 router.get('/', (_req, res) => {
     res.json({
@@ -13,13 +17,21 @@ router.get('/', (_req, res) => {
 });
 
 router.get('/tracks', (_req, res) => {
-    readdir('static').then(files => {
-        const tracks = files.filter(file => {
+    readdir(tracksFolder).then(files => {
+        const trackPromises = files.filter(file => {
             return file.endsWith('.mp3');
         }).map(file => {
-            return { file };
+            const fullPath = path.join(tracksFolder, file);
+            return musicMetadata.parseFile(fullPath).catch(err => {
+                console.log('Failed to parse metadata for ', fullPath, ' error ', err);
+            });
         });
-        res.json({ tracks });
+        Promise.all(trackPromises).then(tracks => {
+            const validTracks = tracks.filter(track => !!track);
+            res.json({
+                tracks: validTracks
+            });
+        });
     }).catch(err => {
         const errorMsg = 'Failed to get tracks';
         res.json({
@@ -28,6 +40,23 @@ router.get('/tracks', (_req, res) => {
             }
         });
         console.log(errorMsg, err);
+    });
+});
+
+router.get('/track', (req, res) => {
+    const track = req.query.id;
+    const file = path.join(tracksFolder, track);
+    exists(file).then(fileExists => {
+        if (fileExists) {
+            const stream = fs.createReadStream(file);
+            stream.pipe(res);
+        } else {
+            res.json({
+                error: {
+                    msg: 'File not found'
+                }
+            });
+        }
     });
 });
 
